41487fc91c55957ea704100c24704648b51ab98a
schragman <schragman@arcor.de>
2018-06-04 18:28:09 +0200

    Erstes mal lauffähig

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,9 +1,17 @@
+package main.java;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
 @Entity
 @NamedQuery(name = "findAllBooks", query = "SELECT b FROM Book b")
 public class Book {
 
 	@Id @GeneratedValue
-	private Long id
+	private Long id;
 	@Column(nullable = false)
 	private String title;
 	private Float price;
@@ -12,6 +20,48 @@ public class Book {
 	private String isbn;
 	private Integer nbOfPages;
 	private Boolean illustrations;
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public Integer getNbOfPages() {
+		return nbOfPages;
+	}
+	public void setNbOfPages(Integer nbOfPages) {
+		this.nbOfPages = nbOfPages;
+	}
+	public Boolean getIllustrations() {
+		return illustrations;
+	}
+	public void setIllustrations(Boolean illustrations) {
+		this.illustrations = illustrations;
+	}
+	public Long getId() {
+		return id;
+	}
+
+
 
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,31 @@
+package main.java;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Main {
-public static void main(String[] args {
-	Book book = new Book();
-	book.setTitle("The Hitchhiker's Guide to the Galaxy");
-	book.setPrice(12.5F);
-	book.setDescription("Science Fiction comedy book");
-	book.setIsbn("1-84023-742-2");
-	book.setNbOfPage(354);
-	book.setIllustrations(false);
+	public static void main(String[] args) {
+		Book book = new Book();
+		book.setTitle("The Hitchhiker's Guide to the Galaxy");
+		book.setPrice(12.5F);
+		book.setDescription("Science Fiction comedy book");
+		book.setIsbn("1-84023-742-2");
+		book.setNbOfPages(354);
+		book.setIllustrations(false);
 
-	//Gets an entity manager and a transaction
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("chapter02PU");
-	EntityManager em = emf.createEntityManager();
+		// Gets an entity manager and a transaction
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("chapter02PU");
+		EntityManager em = emf.createEntityManager();
 
-	//Persists the book to the database
-	EntityTransaction tx = em.getTransaction();
-	tx.begin();
-	em.persist(book);
-	tx.commit();
+		// Persists the book to the database
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		em.persist(book);
+		tx.commit();
 
-	em.close();
-	emf.close();
+		em.close();
+		emf.close();
 	}
-	}
-
+}
